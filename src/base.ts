df6fac1d461b1e2ffa2f1dc60e6590be96a04414
Ty Busby <tybusby@gmail.com>
2025-08-13 18:19:27 -0400

    checkpoint

--- src/base.ts
+++ src/base.ts
@@ -14,4 +14,86 @@
  * limitations under the License.
  */
 
-export class WorkerBase {}
+import { ContentType } from "content-types-lite";
+import { getReasonPhrase, StatusCodes } from "http-status-codes";
+import { Method } from "./methods";
+
+export abstract class WorkerBase {
+    constructor(
+        protected readonly env: Env,
+        protected readonly ctx: ExecutionContext
+    ) {}
+
+    public async fetch(request: Request): Promise<Response> {
+        switch (request.method) {
+            case "OPTIONS":
+                return await this.options(request);
+            case "GET":
+                return await this.get(request);
+            default:
+                return this.getResponse(StatusCodes.BAD_REQUEST, "");
+        }
+        return new Response();
+    }
+
+    protected async get(_request: Request): Promise<Response> {
+        return this.getResponse(StatusCodes.NOT_IMPLEMENTED);
+    }
+
+    protected async head(_request: Request): Promise<Response> {
+        return this.get(_request);
+    }
+
+    protected async options(_request: Request): Promise<Response> {
+        return this.getResponse(StatusCodes.NO_CONTENT);
+    }
+
+    public getResponse(
+        code: StatusCodes,
+        body: string | null = null,
+        contentType: ContentType = "JSON"
+    ) {
+        const headers = new Headers({
+            // TODO: figure out what to do with Cache-Control
+            // "Cache-Control": "public, max-age=86400, immutable",
+            "X-Content-Type-Options": "nosniff",
+        });
+
+        if (body) {
+            const bodyBytes = new TextEncoder().encode(body);
+            headers.set("Content-Type", contentType);
+            headers.set("Content-Length", bodyBytes.length.toString());
+        }
+
+        return new Response(body, {
+            status: code,
+            statusText: getReasonPhrase(code),
+            headers: this.addCorsHeaders(headers),
+        });
+    }
+
+    protected getAllowOrigin(): string {
+        return "*";
+    }
+
+    protected getAllowMethods(): Method[] {
+        return ["GET", "OPTIONS", "HEAD"];
+    }
+
+    protected getAllowHeaders(): string[] {
+        return ["Content-Type"];
+    }
+
+    private addCorsHeaders(headers: Headers): Headers {
+        headers.set("Access-Control-Allow-Origin", this.getAllowOrigin());
+        headers.set(
+            "Access-Control-Allow-Headers",
+            this.getAllowHeaders().join(". ")
+        );
+        headers.set(
+            "Access-Control-Allow-Methods",
+            this.getAllowMethods().join(", ")
+        );
+        return headers;
+    }
+}
