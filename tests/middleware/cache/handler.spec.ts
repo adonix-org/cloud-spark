92c0e1e19866a775eda353929935eee612cb2ea1
Ty Busby <tybusby@gmail.com>
2025-10-02 15:11:40 -0400

    refactor

--- tests/middleware/cache/handler.spec.ts
+++ tests/middleware/cache/handler.spec.ts
@@ -17,7 +17,7 @@
 import { describe, expect, it } from "vitest";
 import { decodeVaryKey, GET_REQUEST, VALID_ORIGIN, VALID_URL } from "@common";
 import { ctx, defaultCache, env, namedCache } from "@mock";
-import { cache, sortSearchParams, stripSearchParams } from "@src/middleware/cache/handler";
+import { cache} from "@src/middleware/cache/handler";
 import { MiddlewareWorker } from "@src/workers/middleware";
 import { GET, Method } from "@src/constants/methods";
 import { getVaryHeader, getVaryKey } from "@src/middleware/cache/utils";
@@ -309,76 +309,5 @@ describe("cache middleware unit tests", () => {
         expect(response.status).toBe(206);
         expect(defaultCache.size).toBe(0);
         expect(namedCache.size).toBe(0);
-    });
-
-    describe("sort search params function", () => {
-        it("does not modify a url with no search parameters", () => {
-            const req = new Request(VALID_URL);
-            expect(sortSearchParams(req).toString()).toBe(VALID_URL);
-        });
-
-        it("retains the single search parameter in a url", () => {
-            const url = `${VALID_URL}?a=1`;
-            const req = new Request(url);
-            expect(sortSearchParams(req).toString()).toBe(url);
-        });
-
-        it("does not modify pre-sorted search parameters", () => {
-            const url = `${VALID_URL}?a=1&b=2&c=3`;
-            const req = new Request(url);
-            expect(sortSearchParams(req).toString()).toBe(url);
-        });
-
-        it("sorts the url search parameters", () => {
-            const url = `${VALID_URL}?&b=2&c=3&a=1`;
-            const req = new Request(url);
-            expect(sortSearchParams(req).toString()).toBe(`${VALID_URL}?a=1&b=2&c=3`);
-        });
-
-        it("sorts and retains search parameters containing duplicate keys", () => {
-            const url = `${VALID_URL}?&b=2&a=4&c=3&a=1`;
-            const req = new Request(url);
-
-            // normalizeUrl sorts keys alphabetically, but preserves the order of duplicate keys.
-            expect(sortSearchParams(req).toString()).toBe(`${VALID_URL}?a=4&a=1&b=2&c=3`);
-        });
-
-        it("ignores the request method and body when generating the key", () => {
-            const req = new Request(`${VALID_URL}?z=2&a=1`, {
-                method: "POST",
-                body: "ignored",
-            });
-            expect(sortSearchParams(req).toString()).toBe(`${VALID_URL}?a=1&z=2`);
-        });
-
-        it("removes the hash if present", () => {
-            const req = new Request(`${VALID_URL}?z=2&a=1#section`);
-            expect(sortSearchParams(req).toString()).toBe(`${VALID_URL}?a=1&z=2`);
-        });
-    });
-
-    describe("strip search params function", () => {
-        it("removes search parameters from a url", () => {
-            const url = `${VALID_URL}?a=1&b=2`;
-            const req = new Request(url);
-            expect(stripSearchParams(req).toString()).toBe(VALID_URL);
-        });
-
-        it("leaves a url without search parameters unchanged", () => {
-            const req = new Request(VALID_URL);
-            expect(stripSearchParams(req).toString()).toBe(VALID_URL);
-        });
-
-        it("removes search params but retains path", () => {
-            const url = `${VALID_URL}path/to/resource?a=1&b=2`;
-            const req = new Request(url);
-            expect(stripSearchParams(req).toString()).toBe(`${VALID_URL}path/to/resource`);
-        });
-
-        it("removes search params and hash if present", () => {
-            const url = `${VALID_URL}?a=1#section`;
-            const req = new Request(url);
-            expect(stripSearchParams(req).toString()).toBe(`${VALID_URL}`);
-        });
     });
 });
