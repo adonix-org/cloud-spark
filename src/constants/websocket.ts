a0b91df38f61a9a8729c8c0d050e604dad47b475
Ty Busby <tybusby@gmail.com>
2025-10-22 01:18:06 -0400

    remove unused codes, update jsdoc

--- src/constants/websocket.ts
+++ src/constants/websocket.ts
@@ -28,16 +28,8 @@ export const WS_MAX_REASON_CHARS = 123;
 /** WebSocket close codes */
 export const CloseCode = {
     NORMAL: 1000,
-    GOING_AWAY: 1001,
-    PROTOCOL_ERROR: 1002,
-    UNSUPPORTED_DATA: 1003,
     NO_STATUS: 1005,
     ABNORMAL: 1006,
-    INVALID_PAYLOAD: 1007,
-    POLICY_VIOLATION: 1008,
-    MESSAGE_TOO_BIG: 1009,
-    MISSING_EXTENSION: 1010,
-    INTERNAL_ERROR: 1011,
     TLS_HANDSHAKE: 1015,
 } as const;
 
